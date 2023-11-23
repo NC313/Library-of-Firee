9d443ee69dd8781fe50b84a0ca40bd595396dd78
Peggy Sabido <peggy_sabido@hotmail.com>
2023-11-22 21:51:27 -0500

    BookClassUpdate

---
+++ Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_of_Fire
+{
+    public class Book
+    {
+        public Book(string _title, string _author, string _status, DateOnly _checkoutdate) 
+        {
+            Title = _title;
+            Author = _author;
+            Status = _status;
+            CheckoutDate = _checkoutdate;
+        }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Status { get; set; }
+        public DateOnly CheckoutDate { get; set; }       
+    }
+}
