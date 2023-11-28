080c13c1dde33feab0adde85b745dba5103c7255
me@angelamiracle.dev <me@angelamiracle.dev>
2023-11-27 20:50:29 -0500

    updated code

--- Book.cs
+++ Book.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace Library_of_Fire
 {
-    internal class Book
+    public class Book
     {
+        public Book(string title, string author, string status, DateTime duedate)
+        {
+            Title = title;
+            Author = author;
+            Status = status;
+            DueDate = duedate;
+        }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Status { get; set; }
+        public DateTime DueDate { get; set; }
+
     }
 }
