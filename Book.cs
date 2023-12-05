3d1fdcacd802f9ca4d5527f4bfa2c8fb3e0725da
Peggy Sabido <peggy_sabido@hotmail.com>
2023-12-04 19:45:07 -0500

    Added ToLower() for check out by and create the working directory

--- Book.cs
+++ Book.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Library_of_Fire
+﻿namespace Library_of_Fire
 {
     public class Book
     {
@@ -27,12 +20,22 @@ namespace Library_of_Fire
 
         public static List<Book> books = new List<Book>();
 
+
+        public static string TryGetAppDirectory()
+        {
+            string LibraryTerminalPath = @"c:\LibraryTerminal\";
+            if (!Directory.Exists(LibraryTerminalPath))
+            {
+                Directory.CreateDirectory(LibraryTerminalPath);
+            }
+            return LibraryTerminalPath;
+        }
         public static void SaveBookList()
         {
-            string fullPath = @"c:\Temp\BookListDownload.csv";
+            string fullPath = $"{TryGetAppDirectory()}BookListDownload.csv";
             var fs = new FileStream(fullPath, FileMode.OpenOrCreate);
             using var stream = new StreamWriter(fs);
-            for (int i = 0; i < Book.books.Count; i++)
+            for (int i = 0; i < books.Count; i++)
             {
                 string printLine = ($"{books[i].BookId},{books[i].Title},{books[i].Author},{books[i].Status},{books[i].DueDate},{books[i].CheckOutBy}");
                 stream.WriteLine(printLine);
